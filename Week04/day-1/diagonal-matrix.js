00724ebe3a52f27bad13ced825af66bfb87f717d
TarNikolett <72967861+TarNikolett@users.noreply.github.com>
2020-11-16 18:50:22 +0100

    modified

--- Week04/day-1/diagonal-matrix.js
+++ Week04/day-1/diagonal-matrix.js
@@ -7,18 +7,32 @@
 //   0 0 1 0
 //   0 1 0 0
 //   1 0 0 0
+//[1,0,0,0]
+//[0,1,0,0]
+//[0,0,1,0]
+//[0,0,0,1]
 
 // Logold ki a mátrixot a konzolra.
 //
 // * a mérete egy változóból érkezzen, így téve dinamikussá a mátrix létrehozását
 // ** ne ijedj meg a kifejezéstől, egy mátrix: tömbök a tömbben
 
-let matrix = [
-    [0, 0, 0, 1],
-    [0, 0, 1, 0],
-    [0, 1, 0, 0],
-    [1, 0, 0, 0]
-  ]
-  
-  console.log(matrix)
-  
+function createFunction(size) {
+  let matrix = [];
+  for (let index = 0; index < size; index++) {
+    let actualRow = [0, 0, 0, 0];
+    actualRow[index]=1;
+    matrix.push(actualRow);
+  }
+  console.log(matrix);
+};
+createFunction(4);
+
+/*let matrix = [
+  [1, 2, 3],
+  [3, 4, 5],
+  [6, 7, 8]
+]
+
+console.log(matrix[0][0])
+// --> [ [ 1, 2, 3 ], [ 3, 4, 5 ], [ 6, 7, 8 ] ]*/
