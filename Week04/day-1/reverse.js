f0b8cfd14a327b2b753e81ae891b199b81466e24
TarNikolett <72967861+TarNikolett@users.noreply.github.com>
2020-11-17 14:36:07 +0100

    reverse.done

--- Week04/day-1/reverse.js
+++ Week04/day-1/reverse.js
@@ -3,7 +3,25 @@
 // - beéptett metódussal
 // - egy átmeneti arrayt létrehozva, és egy ciklus segítségével
 // Logold ki a konzolra a megfordított numbers tömb mindegyik elemét
-
 'use strict';
 
+// beepitett metodussal
+
+/*
 let numbers = [3, 4, 5, 6, 7];
+let reverseNumbers = numbers.reverse();
+
+console.log(reverseNumbers);
+
+*/
+
+
+//atmeneti array + ciklus
+
+let numbers = [3, 4, 5, 6, 7];
+let reverseNumberList = [];
+
+for (let i = 0; i <= numbers.length -1; i++){
+        reverseNumberList.push(numbers[numbers.length-1-i]);
+    };
+console.log(reverseNumberList);
