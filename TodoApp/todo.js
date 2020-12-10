d42d67b12857f4e1d8b02ed2e7d8504058cee37c
TarNikolett <you@example.com>
2020-12-10 19:04:17 +0100

    update

--- TodoApp/todo.js
+++ TodoApp/todo.js
@@ -1,21 +1,37 @@
-import minimist from 'minimist';
-
-let args = minimist(process.argv);
-console.log(args);
-
 /* Adott a megnyitott terminál a projekt könyvtáron belül
 Amikor elindítom az alkalmazást argumentum nélkül
 Akkor ki kell nyomtatnia az applikáció "használati utasítását"
-$ todo
-
-Parancssori Todo applikáció
-=============================
-
-Parancssori argumentumok:
-    -l   Kilistázza a feladatokat
-    -a   Új feladatot ad hozzá
-    -r   Eltávolít egy feladatot
-    -c   Teljesít egy feladatot
  */
 
- 
+import minimist from 'minimist';
+
+function todo (){
+    console.log( 
+        `Parancssori argumentumok: \n-l   Kilistázza a feladatokat \n-a  Új feladatot ad hozzá \n-r  Eltávolít egy feladatot \n-c  Teljesít egy feladatot`
+    )
+};
+
+/*
+## Tennivalok kilistazasa
+**Adott** a megnyitott terminál a projekt könyvtáron belül
+*És* a fájl, ahol tároljuk a tennivalókat
+*Adj* hozzá egy tennivalót a fájlhoz ezzel a leírással: Kutyát sétáltatni
+*Adj* hozzá egy tennivalót a fájlhoz ezzel a leírással: Tejet venni
+*Adj* hozzá egy tennivalót a fájlhoz ezzel a leírással: Megcsinálni a leckét
+**Amikor** az applikációt az *-l* argumentummal futtatjuk
+**Akkor** nyomtassa ki a konzolra a tennivalókat a fájlból, ahol tároljuk őket
+*És* adjon eléjük sorszámot
+*/
+
+import * as fs from 'fs';
+
+let args = minimist(process.argv);
+
+function listOfThings (args){
+        if ( args === '-l'){
+            console.log( fs.readFileSync('todos.txt').toString());
+        }
+    
+};
+
+listOfThings('-l');
