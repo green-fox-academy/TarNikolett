aef146119fc2de97d1973ec91a04d5f5f7119bc0
TarNikolett <you@example.com>
2020-12-10 14:37:41 +0100

    remake

--- TodoApp/todo.js
+++ TodoApp/todo.js
@@ -2,3 +2,20 @@ import minimist from 'minimist';
 
 let args = minimist(process.argv);
 console.log(args);
+
+/* Adott a megnyitott terminál a projekt könyvtáron belül
+Amikor elindítom az alkalmazást argumentum nélkül
+Akkor ki kell nyomtatnia az applikáció "használati utasítását"
+$ todo
+
+Parancssori Todo applikáció
+=============================
+
+Parancssori argumentumok:
+    -l   Kilistázza a feladatokat
+    -a   Új feladatot ad hozzá
+    -r   Eltávolít egy feladatot
+    -c   Teljesít egy feladatot
+ */
+
+ 
