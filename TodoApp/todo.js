88648abe89463f2fe3ccb279632bc76d180a58d0
TarNikolett <you@example.com>
2020-12-10 14:10:09 +0100

    babysteps

--- TodoApp/todo.js
+++
@@ -1,5 +0,0 @@
-/* Hasznalat megjelenitese
-Adott a megnyitott terminál a projekt könyvtáron belül
-Amikor elindítom az alkalmazást argumentum nélkül
-Akkor ki kell nyomtatnia az applikáció "használati utasítását" */
-
