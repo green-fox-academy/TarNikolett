86eb7680d5a903e2fea821095f5cc098b15a7471
TarNikolett <you@example.com>
2020-12-13 17:19:51 +0100

    9taskdone

--- TodoApp/todo.js
+++ TodoApp/todo.js
@@ -1,7 +1,6 @@
 import minimist from 'minimist';
 import * as fs from 'fs';
-
-//## Használat megjelenítése 
+import { type } from 'os';
 
 function writeArgs() {
     console.log(
@@ -15,47 +14,56 @@ function writeArgs() {
 writeArgs();
 
 let args = minimist(process.argv);
+let listOfThing = fs.readFileSync('todos.txt').toString().split('\n');
 
 function toDo() {
-    let listOfThing = fs.readFileSync('todos.txt').toString().split('\n');
     if (args.l === true) {
-        //## Tennivalok kilistazasa
-
         if (listOfThing != '') {
-            writeThingsToDo()
+            for (let i = 0; i < listOfThing.length; i++) {
+                console.log(i + 1 + ' ' + listOfThing[i]);
+            }
         }
-        //## Ures lista
-
         if (listOfThing == '') {
             console.log('Nincs mára tennivalód! :)')
         }
-    }
-    //## Uj tennivalo hozzaadasa
-
-    else if (args.a !== true || false) {
+    } else if (typeof args.a == 'string') {
         addThingsToDo()
     }
-    //## Uj tennivalo hozzaadasa - hibakezeles
-
     try {
-        if (args.a == true || false) {
+        if (args.a && typeof args.a !== 'string') {
             throw Error('Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!')
+        }
+        if (args.r == true) {
+            throw Error('Nem lehetséges az eltávolítás: nem adott meg indexet!')
+        }
+        if (args.r && typeof args.r !== 'number') {
+            throw Error('Nem lehetséges az eltávolítás: a megadott index nem szám!')
+        }
+        if (args.r > listOfThing.length) {
+            throw Error('Nem lehetséges az eltávolítás: túlindexelési probléma adódott!');
+        }
+        if (args.keys !== 'l', 'a', 'r', 'c') {
+            throw Error('Nem támogatott argumentum! Nyomtassa ki az applikáció "használati utasítását".')
         }
     }
     catch (err) {
-        console.log(err.message)
+        console.log(err.message);
     }
 };
-
 toDo();
-
-function writeThingsToDo() {
-    let listOfThing = fs.readFileSync('todos.txt').toString().split('\n');
-    for (let i = 0; i < listOfThing.length; i++) {
-        console.log(i + 1 + ' ' + listOfThing[i]);
-    }
-};
 
 function addThingsToDo() {
     fs.appendFileSync('todos.txt', '\n' + (args.a))
 };
+
+// ## Tennivalo eltavolitasa
+/* **Adott** a megnyitott terminál a projekt könyvtáron belül
+*És* a fájl, ahol tároljuk a tennivalókat
+*És* a fájlban 2 vagy több tennivaló van elmentve
+**Amikor** az applikációt az -r 2 argumentummal futtatjuk
+**Akkor** az app távolítsa el a második tennivalónkat a fájlból */
+
+function removeThingsToDo() {
+    let donetodo = listOfThing.splice(args.r - 1, 1);
+    fs.writeFileSync('todos.txt', listOfThing.toString());
+}; 
