c50df48dc081cd457b0c0f0b0e7d74a6823c15c7
TarNikolett <you@example.com>
2021-01-04 17:59:46 +0100

    refresh

--- examPractice/banksystem.js
+++ examPractice/banksystem.js
@@ -1,9 +1,9 @@
 class Currency {
     code;
     centralBankName;
-    value; 
+    value;
 
-    constructor (code, centralBankName, value){
+    constructor(code, centralBankName, value) {
         this.code = code;
         this.centralBankName = centralBankName;
         this.value = value;
@@ -20,8 +20,8 @@ class bankAccount {
         this.currency = currency;
     }
 
-    deposit ( value ){
-        if ( value >= 0 ){
+    deposit(value) {
+        if (value >= 0) {
             return this.currency += value;
         }
         else {
@@ -29,8 +29,8 @@ class bankAccount {
         }
     }
 
-    withdraw (pinCode, amount){
-        if ( pinCode = this.pinCode && this.currency > amount){
+    withdraw(pinCode, amount) {
+        if (pinCode = this.pinCode && this.currency > amount) {
             return this.currency -= amount;
         }
         else {
@@ -41,34 +41,34 @@ class bankAccount {
 
 class bank {
     bankAccountList = [
-        new bankAccount ( 'David' , 1000),
-        new bankAccount ( 'Cody' , 2000)
+        new bankAccount('David', 1000),
+        new bankAccount('Cody', 2000)
     ];
 
-    createAccount( bankAccount )  {
+    createAccount(bankAccount) {
         this.bankAccountList.push(bankAccount);
     }
 
-    getAllMoney (){
-       let sum = [];
-         for ( let i = 0; i < this.bankAccountList.length; i++){
+    getAllMoney() {
+        let sum = [];
+        for (let i = 0; i < this.bankAccountList.length; i++) {
             sum.push(this.bankAccountList[i].currency);
-        } 
-        return sum.reduce( (a , b) => a + b, 0)
+        }
+        return sum.reduce((a, b) => a + b, 0)
     }
 };
-let USADollar = new Currency ( 'USD' , 'Federal Reserve System' , 300 );
-let HUFForint = new Currency ( 'HUF' , 'Hungarian National Bank' , 350);
+let USADollar = new Currency('USD', 'Federal Reserve System', 300);
+let HUFForint = new Currency('HUF', 'Hungarian National Bank', 350);
 console.log(HUFForint, USADollar);
 
- let NikiBankAccount = new bankAccount ( 'Niki' , 3500);
+let NikiBankAccount = new bankAccount('Niki', 3500);
 console.log(NikiBankAccount);
 NikiBankAccount.deposit(200);
 console.log(NikiBankAccount);
 NikiBankAccount.withdraw(1995, 200);
 console.log(NikiBankAccount);
 
-let trial = new bank ();
+let trial = new bank();
 console.log(trial);
 trial.createAccount(NikiBankAccount);
 console.log(trial);
