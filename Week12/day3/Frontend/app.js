44fc7ba1618013f61b9dd6999d3ca6bb67619e18
TarNikolett <you@example.com>
2021-01-31 19:08:32 +0100

    Frontend task almost done - do until missing

---
+++ Week12/day3/Frontend/app.js
@@ -0,0 +1,86 @@
+import express from 'express';
+import { type } from 'os';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(express.static('./assets'));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.hu.html`);
+});
+
+app.get('/doubling', (req, res) => {
+    let num = duplication(req.query.input);
+    if (req.query.input == undefined) {
+        res.send({
+            "error": "Please provide an input!"
+        });
+    }
+    res.send({
+        received: parseInt(req.query.input),
+        result: num
+    });
+});
+
+function duplication(num) {
+    let duplicatedNumber = num * 2;
+    return duplicatedNumber;
+};
+
+app.get('/greeter', (req, res) => {
+    let name = req.query.name;
+    let title = req.query.title;
+    let answer;
+    if (name == undefined && title !== undefined) {
+        answer = {
+            "error": "Please provide a name!"
+        }
+        res.writeHead(400);
+    }
+    else if (title == undefined && name !== undefined) {
+        answer = {
+            "error": "Please provide a title!"
+        }
+        res.writeHead(400);
+    }
+    else if (name == undefined && title == undefined) {
+        answer = {
+            "error": "Please provide a name and a title!"
+        }
+        res.writeHead(400);
+    }
+    else {
+        res.send(
+            {
+                name: name,
+                title: title,
+                "welcome_message": `Oh, hi there ${name}, my dear ${title}!`
+            }
+        )
+    };
+    res.end(JSON.stringify(answer));
+});
+
+app.get('/appenda/:appendable', (req, res) => {
+    let obj = {
+        appended : req.params.appendable
+    };
+    obj.appended = appendA(req.params.appendable);
+    console.log(obj);
+    res.send({
+        appended : obj.appended
+    });
+});
+
+function appendA(word) {
+    let addLetter = "a";
+    return word.concat(addLetter)
+};
+
+app.listen(port, () => {
+    console.log(`A szerver fut a ${port} csatornán.`);
+});
