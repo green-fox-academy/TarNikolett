0bc6ec7810123f1ca6fce8e80c594a1f621384d8
TarNikolett <you@example.com>
2021-02-01 17:37:42 +0100

    arrayhandler done

--- Week12/day3/Frontend/app.js
+++ Week12/day3/Frontend/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { type } from 'os';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
