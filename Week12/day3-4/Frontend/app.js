9c59de7b7e90ad8692bbfa2d601adbeada1936dc
TarNikolett <you@example.com>
2021-02-21 19:01:53 +0100

    refresh

--- Week12/day3-4/Frontend/app.js
+++ Week12/day3-4/Frontend/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { runInNewContext } from 'vm';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
--- Week13/day3/public/app.js
+++ Week13/day3/public/app.js
@@ -1,1 +1,12 @@
-console.log("Működik");
+
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyCVVhJL22gI3BZp_CzurQeiqhExZ8BwIIY",
+    authDomain: "logintest-2b72e.firebaseapp.com",
+    projectId: "logintest-2b72e",
+    storageBucket: "logintest-2b72e.appspot.com",
+    messagingSenderId: "49229035865",
+    appId: "1:49229035865:web:c47bfac8086321fe523448"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
