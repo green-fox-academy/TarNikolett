f735095ea23b0044bd3ebf6b4fabc2e9899812a7
TarNikolett <you@example.com>
2021-03-05 12:35:43 +0100

    update with validation

--- Week17/day3/user-registration/src/App.js
+++ Week17/day3/user-registration/src/App.js
@@ -1,60 +1,102 @@
 import { useState, useRef, useEffect } from 'react';
 import './App.css';
 
-function App() {
+let textForErrorMessages = {
+  username: 'Felhasználónév megadása kötelező!',
+  email: 'A mezőnek tartalmaznia kell a @ karaktert!',
+  password: 'A jelszó hossza minimum 8 karakter!'
+}
 
-  let [data, setData] = useState({
+function App() {
+  let [formValues, setFormValues] = useState({
     username: '',
     email: '',
     password: ''
   });
 
-  function handleDataChange(e) {
-    setData({
-      ...data, [e.target.name]: e.target.value
-    })
-  }
-
-  function consoleData(e) {
-    e.preventDefault();
-    console.log(data)
-  }
+  let [errorMessages, setErrorMessages] = useState({
+    username: '',
+    email: ''
+  });
 
   let usernameInput = useRef(null);
   useEffect(() => {
     usernameInput.current.focus();
   }, []);
 
+  function validateField(value, fieldName) {
+    let isValid = value.length > 0;
+
+    if (fieldName !== 'email') {
+      setErrorMessages({
+        ...errorMessages,
+        [fieldName]: isValid ? '' : textForErrorMessages[fieldName]
+      })
+    }
+    else if (fieldName === 'email' && !value.includes('@')) {
+      setErrorMessages({
+        ...errorMessages,
+        [fieldName]: textForErrorMessages[fieldName]
+      });
+    }
+  }
+
+  function handleValueChange(e) {
+    e.preventDefault();
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+    console.log(formValues)
+  };
+
   return (
     <div className="container">
 
-      <form onSubmit={consoleData}>
+      <form onSubmit={handleValueChange}>
 
         <label htmlFor="username">Felhasználónév</label>
         <input
           type="text"
+          className={`input ${errorMessages.username ? 'invalid' : ''}`}
           id="username"
           name="username"
+          value={formValues.username}
           ref={usernameInput}
-          onChange={handleDataChange}
+          onChange={handleValueChange}
+          onBlur={e => validateField(e.target.value, 'username')}
         />
+        {errorMessages.username && (
+          <div className="alert">{errorMessages.username}</div>
+        )}
 
         <label htmlFor="email">Email cím</label>
         <input
           type="email"
+          className={`input ${errorMessages.email ? 'invalid' : ''}`}
           id="email"
+          value={formValues.email}
           name="email"
-          onChange={handleDataChange}
+          onChange={handleValueChange}
+          onBlur={e => validateField(e.target.value, 'email')}
         />
+        {errorMessages.email && (
+          <div className="alert">{errorMessages.email}</div>
+        )}
 
         <label htmlFor="password">Jelszó</label>
         <input
           type="password"
           id="password"
+          className={`input ${errorMessages.password ? 'invalid' : ''}`}
           name="password"
-          onChange={handleDataChange}
+          value={formValues.password}
+          onChange={handleValueChange}
+          onBlur={e => validateField(e.target.value, 'password')}
         />
-
+        {errorMessages.password && (
+          <div className="alert">{errorMessages.password}</div>
+        )}
         <button type="submit" >Küldés</button>
 
       </form>
